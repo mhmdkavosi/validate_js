7f48145b922d6ba91e5aa55fd5025b949f815bb3
mhmdkavosi <kavosi1010@gmail.com>
2020-10-02 21:14:13 +0330

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'validation.js',
+        filename: 'Validate.js',
         path: path.resolve(__dirname, 'dist')
     },
 };
